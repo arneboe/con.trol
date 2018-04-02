4e6711bfcb06f1d2478f34aaf1331f89ca284a7b
arne <arne@rnae.de>
2018-04-02 21:57:03 +0200

    better analog filtering

--- test/SW4STM32/test/Application/User/fader/Fader.cpp
+++ test/SW4STM32/test/Application/User/fader/Fader.cpp
@@ -1,36 +1,18 @@
 #include <fader/Fader.h>
 #include <hardware/ErrorHandler.h>
 #include <hardware/Hardware.h>
-#include <cmath>
 #include "Map.h"
 
 Fader Faders::faders[NUM_FADERS];
 //NOTE this is later casted to uint32* but the dma is configured to write only half-words
 static uint16_t adcValues[NUM_FADERS] = { 0 };
 
-// affects the curve of movement amount > snap amount
-// smaller amounts like 0.001 make it ease slower
-// larger amounts like 0.1 make it less smooth
-//from http://damienclarke.me/code/posts/writing-a-better-noise-reducing-analogread
-#define SNAP_MULTIPLIER 0.035f
-
-float snapCurve(uint16_t x)
-// now calculate a 'snap curve' function, where we pass in the diff (x) and get back a number from 0-1. We want small values of x to result in an output close to zero, so when the smooth value is close to the input value it'll smooth out noise aggressively by responding slowly to sudden changes. We want a small increase in x to result in a much higher output value, so medium and large movements are snappy and responsive, and aren't made sluggish by unnecessarily filtering out noise. A hyperbola (f(x) = 1/x) curve is used. First x has an offset of 1 applied, so x = 0 now results in a value of 1 from the hyperbola function. High values of x tend toward 0, but we want an output that begins at 0 and tends toward 1, so 1-y flips this up the right way. Finally the result is multiplied by 2 and capped at a maximum of one, which means that at a certain point all larger movements are maximally snappy
-{
-  float y = 1.0f / (x + 1);
-  y = (1 - y) * 2;
-  if(y > 1) {
-    return 1;
-  }
-  return y;
-};
-
 
 /**taken from arduino: used for log to lin conversion
   * https://playground.arduino.cc/Main/MultiMap */
 int multiMap(uint16_t value)
 {
-  //adc has only 12 bit resolution, everything above has to be garbage and shouldnt be there
+  //adc has only 12 bit resolution, everything above has to be garbage and shouldn't be there
   if(value > 4095) value = 4095;
 
 //  static uint16_t potiVals[] = {0, 44, 120, 222, 345, 470, 590, 895, 1715, 2465, 3220, 4050, 4094, 4095};
@@ -51,28 +33,22 @@ int multiMap(uint16_t value)
 }
 
 
-//from http://damienclarke.me/code/posts/writing-a-better-noise-reducing-analogread
 void Fader::update(uint16_t adcValue)
 {
-  // get difference between new input value and current smooth value
-  const float diff = fabs((float)adcValue - averageAdcValue);
-
-  // multiply the input by SNAP_MULTIPLER so input values fit the snap curve better.
-  const float snap = snapCurve(diff * SNAP_MULTIPLIER);
-  // calculate the exponential moving average based on the snap
-  averageAdcValue += (adcValue - averageAdcValue) * snap;
+//  avg = 0.5f * avg + 0.5f * adcValue;
+  filter.update(adcValue);
 }
 
 uint8_t Fader::getLinearMidiValue()
 {
-  const uint8_t midiValue = multiMap((int)(averageAdcValue + 0.5f));
+  const uint8_t midiValue = multiMap(filter.getValue());
   if(midiValue > 127) return 127; //happens because mapping from log to lin is not perfect (different faders have different maximums)
   return midiValue;
 }
 
 uint8_t Fader::getLogMidiValue()
 {
-  const uint8_t midiValue = map((int)(averageAdcValue + 0.5f), 0, 4085, 0, 127);
+  const uint8_t midiValue = map(filter.getValue(), 0, 4085, 0, 127);
   //we use 4085 as maximum because most faders cant reach 4096,
   if(midiValue > 127) return 127;//happens if value is > 4085
   return midiValue;
