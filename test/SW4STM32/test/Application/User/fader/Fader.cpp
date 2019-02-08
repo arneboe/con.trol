8d6aa6d23c1f3fee9e302f0bacb8b41b3512299d
Arne Böckmann <arne@rnae.de>
2019-02-08 13:48:29 +0100

    replaced log fader with lin faders

--- test/SW4STM32/test/Application/User/fader/Fader.cpp
+++ test/SW4STM32/test/Application/User/fader/Fader.cpp
@@ -8,17 +8,15 @@ Fader Faders::faders[NUM_FADERS];
 static uint16_t adcValues[NUM_FADERS] = { 0 };
 
 
-/**taken from arduino: used for log to lin conversion
-  * https://playground.arduino.cc/Main/MultiMap */
-int multiMap(uint16_t value)
+/** maps from linear to logarithmic fader scale
+ * modified from: https://playground.arduino.cc/Main/MultiMap */
+int linToLog(uint16_t value)
 {
   //adc has only 12 bit resolution, everything above has to be garbage and shouldn't be there
   if(value > 4095) value = 4095;
 
-//  static uint16_t potiVals[] = {0, 44, 120, 222, 345, 470, 590, 895, 1715, 2465, 3220, 4050, 4094, 4095};
-  static float potiVals[] = {0, 82,164, 573, 655, 737, 4014, 4095};
-//  static uint16_t midiVals[] = {0, 10, 21, 31, 42, 52, 63, 74, 84, 95, 105, 116, 127, 127};
-  static float midiVals[] = {0, 25, 38, 76, 81, 83, 121, 127};
+  static float potiVals[] = {0, 516, 741, 870, 999, 1096, 1193, 1257, 1354, 1419, 1483, 1548, 1741, 1935, 2128, 2547, 3031, 3547, 4095};
+  static float midiVals[] = {0, 1  , 2  , 3  , 4  , 5   , 6   , 7   , 8   , 9   , 10  , 11  , 15  , 20  , 25  , 39  , 60  , 89  , 127};
 
   // search right interval
   uint8_t pos = 0;
@@ -40,17 +38,15 @@ void Fader::update(uint16_t adcValue)
 
 uint8_t Fader::getLinearMidiValue()
 {
-  const uint8_t midiValue = multiMap(filter.getValue());
-  if(midiValue > 127) return 127; //happens because mapping from log to lin is not perfect (different faders have different maximums)
+  const uint8_t midiValue = map(filter.getValue(), 0, 4093, 0, 127);
+  //we use 4093 as maximum because most faders cant reach 4096,
+  if(midiValue > 127) return 127;//happens if value is > 4093
   return midiValue;
 }
 
 uint8_t Fader::getLogMidiValue()
 {
-  const uint8_t midiValue = map(filter.getValue(), 0, 4085, 0, 127);
-  //we use 4085 as maximum because most faders cant reach 4096,
-  if(midiValue > 127) return 127;//happens if value is > 4085
-  return midiValue;
+  return linToLog(filter.getValue());
 }
 
 uint8_t Fader::getMidiValue()
@@ -71,24 +67,6 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
   //start next reading
   if (HAL_ADC_Start_DMA(&hadc1, (uint32_t*) adcValues, NUM_FADERS) != HAL_OK)
     Error_Handler();
-
-
-  /*
-  static uint8_t oldMidi[NUM_FADERS] = {0};
-
-  for(int i = 0; i < NUM_FADERS; ++i)
-  {
-    const uint8_t val = Faders::faders[i].getLogMidiValue();
-    if(oldMidi[i] != val)
-    {
-      oldMidi[i] = val;
-      printf("%d: %d\n",i, val);
-    }
-
-
-  }
-  */
-
 }
 
 void Faders::start()
